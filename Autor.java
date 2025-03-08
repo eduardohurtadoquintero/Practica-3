61386a3c84ad26f3d3a6df7c36853eddf3633ddf
Eduardo Hurtado Quintero <e1190534@uabc.edu.mx>
2025-03-07 21:45:36 -0800

    commit 1

---
+++ Autor.java
@@ -0,0 +1,39 @@
+public class Autor {
+
+    private int id;
+    private String nombre;
+    private String correo;
+
+    // Constructor
+    public Autor(int id, String nombre, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    // Métodos getter
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    // Métodos setter (si es necesario)
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
